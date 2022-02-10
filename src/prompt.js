7a3ef6b523868eb9d30a59ca1b02ba7f4cede4af
Cenk Kılıç <cenk@kilic.dev>
2022-02-10 23:06:19 +0100

    fix: update edit

--- src/prompt.js
+++ src/prompt.js
@@ -15,7 +15,7 @@ export class EditorPrompt extends Prompt {
 
   // Intercept submit and launch the editor
   async submit () {
-    this.value = await edit(this.value)
+    this.value = edit(this.value)
 
     return super.submit()
   }
